9d935aa2592053495661048ce18f15e6c89b1968
huangshixin <465750823@qq.com>
2022-06-10 15:36:56 +0800

    fix: test

--- ProjectMange/Startup.cs
+++ ProjectMange/Startup.cs
@@ -21,7 +21,6 @@ namespace ProjectMange
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
             services.AddControllers();
             /*配置JWT认证中心*/
             //读取一波配置文件
